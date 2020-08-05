134b8768a6f3ecd4265484456301b3bab6af3019
Kallaf <kalaf1997@yahoo.com>
2020-08-05 15:51:22 +0200

    clean the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GameControlComponent} from './game-control/game-control.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
